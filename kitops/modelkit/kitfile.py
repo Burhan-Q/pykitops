86910dbe9a25e532f69379b9d3558b90c7298722
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2025-02-20 10:01:20 -0500

    refactor: Uses `pathlib.Path` method in `kitfile.py` (#25)
    * remove context manager, use pathlib.Path, + specify encoding
    
    * include encoding keyword + intro walrus operator for error parsing conditional
    
    Signed-off-by: Burhan <62214284+Burhan-Q@users.noreply.github.com>

--- kitops/modelkit/kitfile.py
+++ kitops/modelkit/kitfile.py
@@ -110,8 +110,10 @@ class Kitfile(PydanticKitfile):
 
         # try to load the kitfile
         try:
-            data: dict = yaml.safe_load(kitfile_path.read_text(encoding="utf-8"))
+            data = yaml.safe_load(kitfile_path.read_text(encoding="utf-8"))
+
         except yaml.YAMLError as e:
+            if mark := getattr(e, "problem_mark", None):
             if mark := getattr(e, "problem_mark", None):
                 raise yaml.YAMLError(
                     "Error parsing Kitfile at " + f"line{mark.line + 1}, " + f"column:{mark.column + 1}."
@@ -170,6 +172,7 @@ class Kitfile(PydanticKitfile):
             >>> kitfile = Kitfile()
             >>> kitfile.save("path/to/Kitfile")
         """
+        Path(path).write_text(self.to_yaml(), encoding="utf-8")
         Path(path).write_text(self.to_yaml(), encoding="utf-8")
 
         if print:
