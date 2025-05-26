803d06cc60b784a34ea78d0804a4ae3c08db7687
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2025-05-26 11:52:14 -0400

    expand test suite with new Pydantic methods, apply ruff formatting
    Signed-off-by: Burhan <62214284+Burhan-Q@users.noreply.github.com>

--- tests/test_kitfile_output.py
+++ tests/test_kitfile_output.py
@@ -6,14 +6,21 @@ from kitops.modelkit.kitfile import Kitfile
 
 def test_initialize_from_path(fixtures: dict[str, str]):
     kitfile = Kitfile(path=str(fixtures["Kitfile_full"]))
+    kitfile_dict = kitfile.model_dump()
+    blank_kitfile = Kitfile()
 
     assert kitfile.manifestVersion == "1.0"
 
     # assert package block
     assert kitfile.package.get("name") == "Titanic-Survivability-Predictor"
-    assert kitfile.package.get("description") == \
-        "A model attempting to predict passenger survivability of  the Titanic Shipwreck"
+    assert (
+        kitfile.package.get("description")
+        == "A model attempting to predict passenger survivability of  the Titanic Shipwreck"
+    )
     assert kitfile.package.get("authors") == ["Jozu"]
+
+    blank_kitfile.package = kitfile_dict["package"]
+    assert kitfile.package == blank_kitfile.package
 
     # assert code block
     assert len(kitfile.code) == 2
@@ -21,9 +28,14 @@ def test_initialize_from_path(fixtures: dict[str, str]):
     assert kitfile.code[0].get("description") == "Python packages required by this example."
     assert kitfile.code[0].get("license") == "Apache-2.0"
     assert kitfile.code[1].get("path") == "titanic_survivability.ipynb"
-    assert kitfile.code[1].get("description") == \
-        "Jupyter Notebook used to train, validate, optimize and  export the model."
+    assert (
+        kitfile.code[1].get("description")
+        == "Jupyter Notebook used to train, validate, optimize and  export the model."
+    )
     assert kitfile.code[1].get("license") == "Apache-2.0"
+
+    blank_kitfile.code = kitfile_dict["code"]
+    assert kitfile.code == blank_kitfile.code
 
     # assert datasets block
     assert len(kitfile.datasets) == 2
@@ -36,12 +48,18 @@ def test_initialize_from_path(fixtures: dict[str, str]):
     assert kitfile.datasets[1].get("description") == "Data to be used for model testing."
     assert kitfile.datasets[1].get("license") == "Apache-2.0"
 
+    blank_kitfile.datasets = kitfile_dict["datasets"]
+    assert kitfile.datasets == blank_kitfile.datasets
+
     # assert docs block
     assert len(kitfile.docs) == 2
     assert kitfile.docs[0].get("path") == "README.md"
     assert kitfile.docs[0].get("description") == "Important notes about the project."
     assert kitfile.docs[1].get("path") == "images"
     assert kitfile.docs[1].get("description") == "Directory containing figures and graphs exported as image files."
+
+    blank_kitfile.docs = kitfile_dict["docs"]
+    assert kitfile.docs == blank_kitfile.docs
 
     # assert model block
     assert kitfile.model.get("name") == "titanic-survivability-predictor"
@@ -50,7 +68,10 @@ def test_initialize_from_path(fixtures: dict[str, str]):
     assert kitfile.model.get("framework") == "joblib"
     assert kitfile.model.get("license") == "Apache-2.0"
     assert kitfile.model.get("version") == "1.0"
-    
+
+    blank_kitfile.model = kitfile_dict["model"]
+    assert kitfile.model == blank_kitfile.model
+
     # assert model parts
     assert len(kitfile.model.get("parts")) == 4
     assert kitfile.model.get("parts")[0].get("path") == "config.json"
@@ -60,10 +81,15 @@ def test_initialize_from_path(fixtures: dict[str, str]):
     assert kitfile.model.get("parts")[2].get("path") == "tokenizer_config.json"
     assert kitfile.model.get("parts")[3].get("path") == "vocab.txt"
 
+    assert kitfile.model.parts == blank_kitfile.model.parts
+
     # assert model parameters
     assert kitfile.model.get("parameters").get("param1") == "val1"
     assert kitfile.model.get("parameters").get("param2") == "val2"
     assert kitfile.model.get("parameters").get("items") == ["list item 1", "list item 2"]
+
+    assert kitfile.model.parameters == blank_kitfile.model.parameters
+
 
 def test_initialize_from_path_and_mutate(fixtures: dict[str, str]):
     current_directory = os.getcwd()
@@ -97,6 +123,7 @@ def test_initialize_from_path_and_mutate(fixtures: dict[str, str]):
     assert kitfile.package.get("version") == "2.0.0"
     assert kitfile.package.get("description") == "New description"
     assert kitfile.package.get("authors") == ["Author"]
+
 
 def test_empty_kitfile_and_mutate():
     # Create an empty Kitfile and update attributes
